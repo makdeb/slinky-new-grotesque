2f38dcbcd4eb0586eba92388ac0f9cc1eaf43f77
yurik <mail4yurik@gmail.com>
2012-07-02 19:20:39 +0300

    search

--- assets/notebook/view/product/Search.js
+++ assets/notebook/view/product/Search.js
@@ -2,33 +2,69 @@ Ext.define('Notebook.view.product.Search',{
     extend: 'Ext.window.Window',
     alias: 'widget.product-search',
     title: 'Поиск',
-    layout: {
-        type: 'hbox',
-        align: 'stretch'
-    },
     resizable: false,
     closable: false,     
     modal: true,
-    width: 450,
-    height: 250, 
-    padding: '5 5 5 5',
+    width: 480,
+    height: 120, 
     items: [
         {
             xtype: 'container',
             layout: 'column',
-            width: 450,
+            width: 470,
+            margin: '5 5 0 5',
             items: [
                 {
                    xtype: 'combobox',
-                   width: 200,
-                   labelWidth: 50,
+                   width: 190,
+                   labelWidth: 40,
+                   margin: '0 5 0 0',
                    fieldLabel: 'Поле',
                    store: 'SearchField',
                    displayField: 'name',
                    valueField: 'id'
-                }               
+                },
+                {
+                    xtype: 'textfield',
+                    labelWidth: 55,
+                    fieldLabel: 'Значение',
+                    width: 260
+                }
             ]
-        }
+        },
+        {
+            xtype: 'container',
+            layout: 'column',
+            width: 470,
+            margin: '5 0 0 5',
+            items: [        
+        {
+            xtype: 'checkbox',
+            width: 120,
+            margin: '0 0 0 45',
+            boxLabel: 'искать за период'
+        },
+//        {
+//            xtype: 'container',
+//            layout: 'column',
+//            width: 450,
+//            items: [
+                {
+                    xtype: 'datefield',
+                    width: 110,
+                    labelWidth: 20,
+                    margin: '0 5 0 65',
+                    fieldLabel: 'С'
+                },
+                {
+                    xtype: 'datefield',
+                    width: 110,
+                    labelWidth: 20,
+                    //margin: '5 0 0 5',
+                    fieldLabel: 'По'
+                }                
+            ]
+        }              
     ],
     buttons: [
         {
