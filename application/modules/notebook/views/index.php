5e889f81adac97e97e2daa1039efa2fbcf6e7937
yura <mail4yurik@gmail.com>
2012-09-12 21:08:54 +0300

    -завантажуэться не дебаг версыя extjs -змынив title ))

--- application/modules/notebook/views/index.php
+++ application/modules/notebook/views/index.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title id="page-title">Account Manager</title>
+    <title id="page-title">DDA</title>
 
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/extjs/resources/css/ext-all.css" />
-    <script type="text/javascript" src="<?php echo base_url();?>assets/extjs/ext-debug.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/extjs/ext.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>assets/extjs/locale/ext-lang-ru.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>assets/Notebook.js"></script>
 </head>
